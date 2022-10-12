6d7299541814f70f89ef291b4031d6b41cf6a20d
Christian Felipe Carvalho <64433548+christianfelipecarvalho@users.noreply.github.com>
2022-10-12 19:56:47 -0300

    Adicionando classes na branch inicial

--- PostoDeSaude/src/Pessoa.java
+++ PostoDeSaude/src/Pessoa.java
@@ -1,3 +1,11 @@
 public class Pessoa {
     private Integer codigo;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private char uf;
+    private String rua;
+    private String pais;
+    private String bairro;
+
 }

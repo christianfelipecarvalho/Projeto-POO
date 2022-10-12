6b92ab9e7e35d4c2b86a92d2b45d78fcae2f4d2a
Christian Felipe Carvalho <64433548+christianfelipecarvalho@users.noreply.github.com>
2022-10-12 20:00:21 -0300

    Adicionando getter e setters

--- PostoDeSaude/src/Pessoa.java
+++ PostoDeSaude/src/Pessoa.java
@@ -8,4 +8,67 @@ public class Pessoa {
     private String pais;
     private String bairro;
 
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public char getUf() {
+        return uf;
+    }
+
+    public void setUf(char uf) {
+        this.uf = uf;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
 }

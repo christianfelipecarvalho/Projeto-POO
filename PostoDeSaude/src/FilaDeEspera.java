756ab4ba118e0ad6c3c528248bf2398a81fee953
Christian Felipe Carvalho <64433548+christianfelipecarvalho@users.noreply.github.com>
2022-10-23 19:17:32 -0300

    Revert "Merge branch 'Criacao_Cadastros' of https://github.com/christianfelipecarvalho/Projeto-POO into Criacao_Cadastros"
    atualizando branch

--- PostoDeSaude/src/FilaDeEspera.java
+++ PostoDeSaude/src/FilaDeEspera.java
@@ -1,17 +1,10 @@
-public enum FilaDeEspera{
-        FILATRIAGEM("Fila triagem"),
-        MUITOURGENTE("Muito Urgente"),
-        URGENTE("Urgente"),
-        POUCOURGENTE("Pouco Urgente"),
-        NAOURGENTE("Não urgente");
-
-        private String Classificacao;
-        FilaDeEspera(String classificacao){
-                this.Classificacao = classificacao;
-
-        }
-
-        public String getClassificacao() {
-                return this.Classificacao;
-        }
+public enum FilaDeEspera {
+        FILATRIAGEM,
+        MUITOURGENTE,
+        URGENTE,
+        POUCOURGENTE,
+        NAOURGENTE;
 }
+
+
+
--- PostoDeSaude/src/Funcionario.java
+++ PostoDeSaude/src/Funcionario.java
@@ -10,8 +10,6 @@ public class Funcionario extends Pessoa{
         ENFERMEIRO,
         MEDICO;
     }
-
-
     public String getCargo() {
         return cargo;
     }

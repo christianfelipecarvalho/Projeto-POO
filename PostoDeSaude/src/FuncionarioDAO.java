9074b9225ec1a00bcb1e19a82c3d374aa3569169
Christian Felipe Carvalho <64433548+christianfelipecarvalho@users.noreply.github.com>
2022-10-24 21:51:14 -0300

    Criando metodos e cadastros

---
+++ PostoDeSaude/src/FuncionarioDAO.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public final class FuncionarioDAO {
+    public static List<Funcionario> funcionarios = new ArrayList<>();
+
+
+    public static List<Funcionario> findFuncionarios() {
+        funcionarios = new ArrayList<>();
+        Funcionario recepcionista = new Funcionario();
+        recepcionista.setCargo(String.valueOf(Funcionario.CargosFuncionarios.RECEPCIONISTA));
+        recepcionista.setSetor("Recepção");
+        recepcionista.setConselho("");
+        recepcionista.setMatricula(1000);
+        recepcionista.setNome("Ana");
+        recepcionista.setCpf("578.656.999-80");
+        recepcionista.getCargo();
+        funcionarios.add(recepcionista);
+
+        Funcionario recepcionista2 = new Funcionario();
+        recepcionista2.setMatricula(1002);
+        recepcionista2.setCargo(String.valueOf(Funcionario.CargosFuncionarios.RECEPCIONISTA));
+        funcionarios.add(recepcionista2);
+
+        Funcionario recepcionista3 = new Funcionario();
+        recepcionista3.setMatricula(1003);
+        recepcionista3.setCargo(String.valueOf(Funcionario.CargosFuncionarios.RECEPCIONISTA));
+        funcionarios.add(recepcionista3);
+        Funcionario enfermeiro = new Funcionario();
+        enfermeiro.setCargo(String.valueOf(Funcionario.CargosFuncionarios.ENFERMEIRO));
+        enfermeiro.setSetor("Triagem");
+        enfermeiro.setConselho("coren");
+        enfermeiro.setMatricula(23434);
+        enfermeiro.setNome("Paula");
+        enfermeiro.setCpf("777.872.247-30");
+
+        funcionarios.add(enfermeiro);
+
+        Medico medico = new Medico();
+        medico.setCargo(String.valueOf(Funcionario.CargosFuncionarios.MEDICO));
+        medico.setSetor("Consultorio");
+        medico.setConselho("crm");
+        medico.setMatricula(3001);
+        medico.setNome("João");
+        medico.setEspecialidades("Ortopedista");
+        funcionarios.add(medico);
+
+        return funcionarios;
+    }
+
+    public static List<Funcionario> findFuncionarios(Funcionario.CargosFuncionarios cargo) {
+        List<Funcionario> funcionariosByCargo = new ArrayList<>();
+        for (Funcionario funcionario : findFuncionarios()) {
+            if(funcionario.getCargo().equals(cargo.toString())){
+                funcionariosByCargo.add(funcionario);
+            }
+        }
+        return funcionariosByCargo;
+    }
+}
--- PostoDeSaude/src/Main.java
+++ PostoDeSaude/src/Main.java
@@ -1,108 +1,68 @@
 import javax.swing.*;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.System.exit;
+
 public class Main {
     public static void main(String[] args) {
-
-        List<Funcionario> funcionarios = new ArrayList<>();
-
-        Funcionario recepcionista = new Funcionario();
-        recepcionista.setCargo(String.valueOf(Funcionario.CargosFuncionarios.RECEPCIONISTA));
-        recepcionista.setSetor("Recepção");
-        recepcionista.setConselho("");
-        recepcionista.setMatricula(1000);
-        recepcionista.setNome("Ana");
-        recepcionista.setCpf("578.656.999-80");
-        recepcionista.getCargo();
-        funcionarios.add(recepcionista);
-
-        Funcionario recepcionista2 = new Funcionario();
-        recepcionista2.setMatricula(1002);
-        recepcionista2.setCargo(String.valueOf(Funcionario.CargosFuncionarios.RECEPCIONISTA));
-        funcionarios.add(recepcionista2);
-
-        Funcionario recepcionista3 = new Funcionario();
-        recepcionista3.setMatricula(1003);
-        recepcionista3.setCargo(String.valueOf(Funcionario.CargosFuncionarios.RECEPCIONISTA));
-        funcionarios.add(recepcionista3);
-        Funcionario enfermeiro = new Funcionario();
-        enfermeiro.setCargo(String.valueOf(Funcionario.CargosFuncionarios.ENFERMEIRO));
-        enfermeiro.setSetor("Triagem");
-        enfermeiro.setConselho("coren");
-        enfermeiro.setMatricula(23434);
-        enfermeiro.setNome("Paula");
-        enfermeiro.setCpf("777.872.247-30");
-
-        funcionarios.add(enfermeiro);
-
-        Medico medico = new Medico();
-        medico.setCargo(String.valueOf(Funcionario.CargosFuncionarios.MEDICO));
-        medico.setSetor("Consultorio");
-        medico.setConselho("crm");
-        medico.setMatricula(3001);
-        medico.setNome("João");
-        medico.setEspecialidades("Ortopedista");
-        funcionarios.add(medico);
-
         List<String> cargos = new ArrayList<>();//funcionarios.stream().map(f -> f.getCargo()).collect(Collectors.toList());
 
-        for (Funcionario funcionario : funcionarios) {
+        for (Funcionario funcionario : FuncionarioDAO.findFuncionarios()) {
             cargos.add(funcionario.getCargo());
         }
 
         List<String> matriculaGeral = new ArrayList<>();
 
-        for (Funcionario funcionarioMatricula : funcionarios) {
+        for (Funcionario funcionarioMatricula : FuncionarioDAO.findFuncionarios()) {
             matriculaGeral.add(String.valueOf(funcionarioMatricula.getMatricula()));
         }
 
-        ///TROCAR PARA SWITCH PARA TRATAR A ENTRADA MELHOR
-
-//        int tipoSistema = 0;
-//        if (menuOpcaoSistemas().toString().equals("RECEPCIONISTA")){
-//            tipoSistema = 0;
-//        } else if (menuOpcaoSistemas().toString().equals("ENFERMEIRO")) {
-//            tipoSistema = 1;
-//        } else if (menuOpcaoSistemas().toString().equals("MEDICO")) {
-//            tipoSistema = 2;
-//        }
-//
-//        switch (tipoSistema){
-//            case 0:
-//                System.out.println("teste");
-//                break;
-//            case 1:
-//                System.out.println("2");
-//                break;
-//            case 2:
-//                System.out.println("3");
-//            default:
-//                throw new IllegalStateException("Unexpected value: " + tipoSistema);
-//        }
 
         if (menuOpcaoSistemas().equals(Funcionario.CargosFuncionarios.RECEPCIONISTA)) {
-            Object[] recepcionistaMatricula = {recepcionista.getMatricula(), recepcionista2.getMatricula(), recepcionista3.getMatricula()};
-            Object confirmacao = JOptionPane.showInputDialog(null,
-                    "Você selecionou Recepcionista? Selecione sua matricula:", "Confirmação", 1,
-                    null, recepcionistaMatricula, " ");
+
+            chamaRecepcionista();
+
+            chamaConfirmacao();
 
             //String[] cadastroPaciente = {"Nome", "CPF", "RG", "Cartão do SUS"};
-            int confirmacaoCadastroPaciente = JOptionPane.showConfirmDialog(null, "Deseja cadastrar o paciente? ");
-            if (confirmacaoCadastroPaciente == 0){
-                chamaCadastroPacientes();
-            }
-            else if (confirmacaoCadastroPaciente == 1) {
-                // retornar ao menu de opções
-                menuOpcaoSistemas();
-            } else if (confirmacaoCadastroPaciente == 2) {
-                menuOpcaoSistemas();
-            }
+
         }
     }
-    private static void chamaCadastroPacientes () {
+
+    private static void chamaRecepcionista() {
+        List<Integer> matriculaFuncionrio = new ArrayList<>();
+        for (Funcionario funcionario : FuncionarioDAO.findFuncionarios(Funcionario.CargosFuncionarios.RECEPCIONISTA)) {
+                matriculaFuncionrio.add(funcionario.getMatricula());
+        }
+        Object[] recepcionistaMatricula = matriculaFuncionrio.toArray();// {recepcionista.getMatricula(), recepcionista2.getMatricula(), recepcionista3.getMatricula()};
+
+        Object confirmacao = JOptionPane.showInputDialog(null,
+                "Você selecionou Recepcionista? Selecione sua matricula:", "Confirmação", 1,
+                null, recepcionistaMatricula, "");
+        if (confirmacao == null) {
+            exit(0);
+        }
+    }
+
+    private static Object menuOpcaoSistemas() {
+        Object[] opcaoInicial = {Funcionario.CargosFuncionarios.RECEPCIONISTA, Funcionario.CargosFuncionarios.ENFERMEIRO,
+                Funcionario.CargosFuncionarios.MEDICO};
+
+        Object selecionado = JOptionPane.showInputDialog(null, "Selecione qual sistema você quer acessar:",
+                "MENU", 1, null, opcaoInicial, " ");
+
+        if(selecionado == Funcionario.CargosFuncionarios.RECEPCIONISTA){
+            chamaRecepcionista();
+        }
+
+        return selecionado;
+    }
+
+    private static void chamaCadastroPacientes() {
         List<Paciente> pacientes = new ArrayList<>();
         Paciente paciente = new Paciente();
         pacientes.add(paciente);
@@ -115,7 +75,7 @@ public class Main {
             case 0: // ir para codigo
                 String codigo = JOptionPane.showInputDialog("Escreva o CÓDIGO do paciente:");
                 paciente.setCodigo(Integer.parseInt(codigo));
-                System.out.println(paciente.getCodigo());
+                pacientes.add(paciente);
                 chamaCadastroPacientes();
                 break;
             case 1:
@@ -174,9 +134,8 @@ public class Main {
                 break;
             case 10:
                 String dataNacimento = JOptionPane.showInputDialog("Escreva a DATA DE NASCIMENTO do paciente:");
-                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/AAAA");
-                String date = formatter.format(dataNacimento);
-                paciente.setDataNascimento(LocalDate.parse(date));
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                paciente.setDataNascimento(LocalDate.parse(dataNacimento, formatter));
                 System.out.println(paciente.getDataNascimento());
                 chamaCadastroPacientes();
                 break;
@@ -186,15 +145,20 @@ public class Main {
 
         }
     }
-    private static Object menuOpcaoSistemas(){
-        Object[] opcaoInicial = {Funcionario.CargosFuncionarios.RECEPCIONISTA,Funcionario.CargosFuncionarios.ENFERMEIRO,
-        Funcionario.CargosFuncionarios.MEDICO};
 
-        Object selecionado = JOptionPane.showInputDialog(null, "Selecione qual sistema você quer acessar:",
-                "MENU", 1, null, opcaoInicial, " ");
-
-        return selecionado;
+    private static Object chamaConfirmacao() {
+        int confirmacaoCadastroPaciente = JOptionPane.showConfirmDialog(null, "Deseja cadastrar o paciente? ");
+        if (confirmacaoCadastroPaciente == 0) {
+            chamaCadastroPacientes();
+        } else if (confirmacaoCadastroPaciente == 1) {
+            // retornar ao menu de opções
+            menuOpcaoSistemas();
+        } else if (confirmacaoCadastroPaciente == 2) {
+            exit(0);
+        }
+        return chamaConfirmacao();
     }
+
 }
 
 
